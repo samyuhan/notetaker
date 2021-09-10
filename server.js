f94a466f023fcb61d9230c6919b1b0faffaf5625
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 08:22:45 -0700

    set up express app and data parsing

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const sql = require("mysql");
 const express = require("express");
+
+var app = express();
+var PORT = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
