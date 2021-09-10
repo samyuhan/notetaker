ce8504ec1e43c7ac495d34793c3027205f3416f9
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 08:24:50 -0700

    add fs, path, db require

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const sql = require("mysql");
+// require dependencies
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const db = require("./db/db")
 
+// set up express app
 var app = express();
 var PORT = 5000;
 
+// JSON formatting
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
