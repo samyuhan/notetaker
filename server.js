fa766498b55c6ca5373aacd1ac1d868e0ea08dfd
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 08:27:26 -0700

    create index.html get route

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const db = require("./db/db")
 
 // set up express app
 var app = express();
-var PORT = 5000;
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
 
 // JSON formatting
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
+
+// get route to get index.html
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
