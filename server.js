b26bd7bd636de130aa1c012a34b87615852db2aa
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 09:27:18 -0700

    add slice to db

--- server.js
+++ server.js
@@ -6,45 +6,28 @@ const db = require("./db/db")
 
 // set up express app
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
-
-// JSON formatting
+// data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+// get notes list
+app.get('/api/notes', (req, res) => {
+    res.json(db.slice(1));
+});
 
 // get route to get index.html
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 // get route for notes
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-})
-
-// get notes list
-app.get("/api/notes", function(req, res) {
-    res.json(db);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-// add new note to db
-app.post("/api/notes", function(req, res) {
-    let newNote = req.body;
-    db.push(newNote);
-    
-    fs.writeFile("db/db.json", JSON.stringify(notes,'\t'), err => {
-        if (err) throw err;
-        return true;
-    });
-});
-
-// get notes by id
-app.get("/api/notes/:id", function(req,res) {
-    res.json(notes[req.params.id]);
-});
-    
 // set up listener
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
